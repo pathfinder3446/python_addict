ecf118e4d281494f82e3ab5aa09108cff30cc9c0
Arif <kanebrian0634@gmail.com>
2022-03-27 15:52:52 +0300

    Number guessing game file was updated

--- number_guessing_game.py
+++ number_guessing_game.py
@@ -1,5 +1,6 @@
 """Write a pyhton code, which is a number guessing game that tries to find the number that will be randomly selected from the number sequence from 1 to 100. and indicate in the code that each user has the right to guess 5, deduct 1 right for each wrong guess"""
 
+# Solution
 
 from random import randint
 numb = randint(1,100)
